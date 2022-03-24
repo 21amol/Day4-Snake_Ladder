e94c2edaedfaa67b046a7d35823647034a1858b1
Amol Ghotale <ghotaleamol@gmail.com>
2022-03-24 14:16:35 +0530

    ADDED: Starting point of player.

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class Main {
+
+    static final int START_POSITION = 0;
+
+    public static void main(String[] args) {
+        // welcome to snake ladder program.
+        System.out.println("Welcome to snake ladder game");
+        System.out.println("Player will start from zero position.");
+    }
+}
